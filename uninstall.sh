856eb7972ccd1491afc8504d5ee18b6c68d2a174
Tim Gray <tgray@125px.com>
2011-08-14 13:04:54 -0400

    Fixed uninstall.sh to work on queue files.

--- uninstall.sh
+++ uninstall.sh
@@ -10,13 +10,21 @@ prefix="${PREFIX:-/usr/local}"
 program_dest="$DESTDIR$prefix/bin"
 doc_dest="$DESTDIR$prefix/share/doc/putmail"
 man_dest="$DESTDIR$prefix/share/man"
+i18n_dest="$DESTDIR$prefix/share/locale"
 
 # Remove the files
 echo "Removing $program_dest/putmail.py ... "
 rm -f "$program_dest/putmail.py"
+rm -f "$program_dest/putmail_enqueue.py"
+rm -f "$program_dest/putmail_dequeue.py"
 
 echo "Removing $doc_dest ... "
 rm -fr "$doc_dest"
 
 echo "Removing $man_dest/man1/putmail.py.1 ... "
 rm -f "$man_dest/man1/putmail.py.1"
+rm -f "$man_dest/man1/putmail-queue.1"
+
+echo "Removing $i18n_dest ... "
+rm -fr "$i18n_dest/es/LC_MESSAGES/putmail_enqueue.py.mo"
+rm -fr "$i18n_dest/es/LC_MESSAGES/putmail_dequeue.py.mo"
